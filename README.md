211293caabe27b6685415bf1ce8f148a97df5769
Reycel Huffman <reycel.huffman@outlook.com>
2022-04-20 17:01:16 -0500

    more content

---
+++ README.md
@@ -0,0 +1,8 @@
+## README-Generator
+    ## This project is about making a quick README.md using node index.js
+    ## My motivation was to generate a README 
+    ## I built this project in order to make README 
+    ## It saves you time
+    ## I learn how to write a README using node index.js
+    
+    
--- index.js
+++ index.js
@@ -5,54 +5,92 @@ const fs = require("fs")
 
 // TODO: Create an array of questions for user input
 inquirer
-.prompt([
-    {
-        type: 'input',
-        name: 'title',
-        message: "Title of your project",
-        },
-    {
-        type: 'input',
-        name: 'description',
-        message: 'What is this project about?',
-        },
-    {
-            type: 'input',
-            name: 'motivation',
-            message: 'What was your motivation?',
-        },
-    {
-            type: 'input',
-            name: 'projectReasoning',
-            message: 'Why did you build this project?',
-        },
-    {
-            type: 'input',
-            name: 'solve',
-            message: 'What problem does it solve?',
-        },  
-    {
-            type: 'input',
-            name: 'learn',
-            message: 'What did you learn?',
-        },
-    ]);
-    .then((response) => {
-        console.log(response);
-        let text = `## ${response.title}
-        ## ${response.description}
-        ## ${response.motivation}
-        ## ${response.projectReasoning}
-        ## ${response.solve}
-        ## ${response.learn}
+    .prompt([
         
-        ${generateText(response)}`
-        ;
-        fs.writeFile("README.md", text, (err) =>
-        err ? console.error(err) : console.log('Success!'))
-    }
+        {
+            type: 'input',
+            name: 'title',
+            message: "Title of your project",
+            },
+        {
+            type: 'input',
+            name: 'description',
+            message: "What is this project about? What was your motivation? What did you learn?",
+            },
+        {
+                type: 'input',
+                name: 'installation',
+                message: 'What are the steps required to install your project?',
+            },
+        {
+                type: 'input',
+                name: 'usage',
+                message: 'Provide instruction and examples for use',
+            },
+        {
+                type: 'input',
+                name: 'credits',
+                message: 'List your collaborators',
+            },  
+        {
+                type: 'input',
+                name: 'license',
+                message: 'List of license',
+            },
+        {
+                type: 'input',
+                name: 'badges',
+                message: 'List of badges',
+            },
+        {
+                type: 'input',
+                name: 'feature',
+                message: 'List of features',
+            },
+        {
+                type: 'input',
+                name: 'contribute',
+                message: 'Create application or package you would like to develop',
+            },
+        {
+                type: 'input',
+                name: 'test',
+                message: 'Run test on your application, make a video of you testing your App',
+            },
 
-    );
+])
+
+.then((response) => {
+    console.log(response);
+    let text = `## title
+${response.Title}
+## decription: 
+${response.description}
+## installation:
+    ${response.installation}
+## usage:
+    ${response.usage}
+## credits:
+    ${response.credits}
+## license:
+    ${response.license}
+## badges:
+    ${response.badges}
+## feature:
+    ${response.feature}
+## contribute:
+    ${response.contribute}
+## test:
+    ${response.test}
+    
+`
+
+        ;
+    fs.writeFile("README.md", text, (err) =>
+    err ? console.error(err) : console.log('Success!'))
+}
+
+);
 
 
     // TODO: Create a function to write README file
